c8a05b9bab6ff054530f37f3127504c447a04cf3
Burhan@429 <burhanmir429@gmail.com>
2023-12-25 09:33:56 +0530

    All issues will be resolved

--- src/main/java/com/project/NewsFeed/model/EventRequest.java
+++ src/main/java/com/project/NewsFeed/model/EventRequest.java
@@ -1,22 +1,18 @@
 package com.project.NewsFeed.model;
 
+
 import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
-
-
-import javax.persistence.Lob;
-import java.util.Base64;
 import java.util.Calendar;
 
 @Data
+
 public class EventRequest {
-    private String eventId;
-    private String eventTitle;
-    private String eventLink;
-    private String eventDescription;
-    private MultipartFile eventImage;
-    private Calendar createdDate;
-    private Calendar updateDate;
+    private Long id;
+    private String title;
+    private String link;
+    private String description;
+    private String photoPath;
+    private Calendar date;
 
 
 
